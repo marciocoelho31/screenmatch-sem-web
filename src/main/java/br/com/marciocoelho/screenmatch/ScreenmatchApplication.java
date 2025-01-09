81a2b95c6e22584681bff2ae9d6294d34ffd6f66
Marcio Coelho <m.coelho.socorro@avanade.com>
2025-01-08 21:41:11 -0300

    primeiro commit

---
+++ src/main/java/br/com/marciocoelho/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,30 @@
+package br.com.marciocoelho.screenmatch;
+
+import br.com.marciocoelho.screenmatch.model.DadosSerie;
+import br.com.marciocoelho.screenmatch.service.ConsumoApi;
+import br.com.marciocoelho.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=Breaking+Bad&apikey=501e6e7e");
+//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
